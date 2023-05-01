2d2966a096e8fafbf8aa6adbd3fe08dacd689089
danielanillo <122710522+danielanillo@users.noreply.github.com>
2023-05-01 09:56:44 -0400

    exemple variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+prenom=Neo #"" si on veut ecrire plus qu'un mot
+echo $prenom
+
+declare -i age=33 #on specifie que notre variable doit etre un chiffre
+declare -r CHEMIN=/bin #constante -r, valeur ne peut etre modifié
+
+echo "Nom script" $0
+echo "Nombre params" $#
+echo "perso: " $1
+echo "perso: " $2
