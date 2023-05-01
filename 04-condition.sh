4ec5941f679990be6e0bfe941de2918c32f9366f
danielanillo <122710522+danielanillo@users.noreply.github.com>
2023-05-01 10:25:39 -0400

    conditions

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
+note=68
 
+#if test....
+#if [...]
 
+if [[ $note -lt 60 ]] #plus petit que
+then
+    echo "desole.. vous echouez"
+elif test $note -eq 60 # -eq egal
+then
+    echo "ouf!"
+else
+    echo "Beau travail"
+fi
 
+read -p "entrez une lettre: " lettre
 
+case $lettre in
+    [[:lower:]])
+        echo "la lettre est en minuscule"
+        ;;  
+    *)
+        echo "autre"
+        ;;        
+esac
 
-
-
+if [[ $lettre != "a" ]]
+then 
+    echo "diff de a"
+fi
